0b9a32f904061a8b9bf931557d7e5b094028c0a6
volosgoto <volosgoto@yahoo.com>
2018-08-30 10:23:15 +0300

    add users data

---
+++ app.js
@@ -0,0 +1,34 @@
+let data = [
+    {
+        name: 'John Doe',
+        age: 32,
+        gender: 'male',
+        lookingFor: 'female',
+        location: 'Boston MA',
+        image: 'https://randomuser.me/api/portraits/men/82.jpg'
+    },
+    {
+        name: 'Sara Smith',
+        age: 25,
+        gender: 'female',
+        lookingFor: 'male',
+        location: 'Denwer CO',
+        image: 'https://randomuser.me/api/portraits/men/80.jpg'
+    },
+    {
+        name: 'Sam Sam',
+        age: 45,
+        gender: 'male',
+        lookingFor: 'female',
+        location: 'New York NY',
+        image: 'https://randomuser.me/api/portraits/men/83.jpg'
+    },
+    {
+        name: 'Mary Mar',
+        age: 38,
+        gender: 'female',
+        lookingFor: 'male',
+        location: 'Los Altos CA',
+        image: 'https://randomuser.me/api/portraits/men/84.jpg'
+    }
+];
